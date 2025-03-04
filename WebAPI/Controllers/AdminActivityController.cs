4f76e1ae67823dd48693f9ffeeef3c3fc612780f
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-03-04 16:45:51 +0100

    Fix uow

---
+++ WebAPI/Controllers/AdminActivityController.cs
@@ -0,0 +1,28 @@
+﻿using Application.CQRS.Users;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AdminActivityController : Controller
+{
+    private IMediator _mediator;
+    public AdminActivityController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _mediator.Send(new GetAllAdminActivitiesQuery()));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        return Ok(await _mediator.Send(new GetAdminActivityByIdQuery { Id = id }));
+    }
+}
---
+++ WebAPI/Controllers/DetachedTimeEntryController.cs
@@ -0,0 +1,28 @@
+﻿using Application.CQRS.Users;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DetachedTimeEntryController : Controller
+{
+    private IMediator _mediator;
+    public DetachedTimeEntryController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _mediator.Send(new GetAllDetachedTimeEntriesQuery()));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        return Ok(await _mediator.Send(new GetDetachedTimeEntryByIdQuery { Id = id }));
+    }
+}
