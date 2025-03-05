23236c090b04f6598a2c68ed419a1cecf4ceb766
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-03-05 11:00:36 +0100

    Add update cmd detached time entries

---
+++ Application/CQRS/DetachedTimeEntries/UpdateCommand.cs
@@ -0,0 +1,48 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.Model.TimeEntries;
+using FluentValidation;
+using MediatR;
+
+namespace Application.CQRS.DetachedTimeEntries
+{
+    public class UpdateCommand : IRequest<DetachedTimeEntryDTO>
+    {
+        public required DetachedTimeEntryDTO DetachedTimeEntry { get; set; }
+    }
+
+    public class UpdateCommandValidator : AbstractValidator<AddCommand>
+    {
+        public UpdateCommandValidator(IUnitOfWork uow)
+        {
+            RuleFor(x => x.DetachedTimeEntry.Id)
+                .NotNull()
+                .WithMessage("Id cannot be empty");
+
+            RuleFor(x => x.DetachedTimeEntry.Category)
+                .NotNull()
+                .WithMessage("Category cannot be empty");
+
+            RuleFor(x => x.DetachedTimeEntry.Date)
+                .NotNull()
+                .WithMessage("Date cannot be empty");
+
+            RuleFor(x => x.DetachedTimeEntry)
+                .Must(x => (x.StartTime.HasValue && x.EndTime.HasValue) || x.Duration.HasValue)
+                .WithMessage("TimeEntry must have StartTime and EndTime, or Duration");
+
+            RuleFor(x => x.DetachedTimeEntry.Description)
+                .NotNull()
+                .WithMessage("Description cannot be empty");
+        }
+    }
+
+    public class UpdateCommandHandler(IUnitOfWork uow, IMapper mapper) : IRequestHandler<UpdateCommand, DetachedTimeEntryDTO>
+    {
+        public async Task<DetachedTimeEntryDTO> Handle(UpdateCommand request, CancellationToken cancellationToken)
+        {
+            await uow.DetachedTimeEntryRepository.Update(request.DetachedTimeEntry.Id!, mapper.Map<DetachedTimeEntry>(request.DetachedTimeEntry));
+            return request.DetachedTimeEntry;
+        }
+    }
+}
