cbbc7f54004333f64c217f656efe4aa4bb035ab8
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-03-05 15:55:42 +0100

    Update mappings

--- Application/Mappings.cs
+++ Application/Mappings.cs
@@ -8,16 +8,26 @@ using Domain.Model.Activities;
 using Domain.Model.TimeEntries;
 using Domain.Model.Users;
 
-namespace Application;
-
 public class Mappings : Profile
 {
     public Mappings()
     {
+        CreateMap<BaseActivity, ActivityDTO>()
+            .Include<Activity, ActivityDTO>();
         CreateMap<Activity, ActivityDTO>();
+
+        CreateMap<BaseActivity, AdminActivityDTO>()
+            .Include<AdminActivity, AdminActivityDTO>();
         CreateMap<AdminActivity, AdminActivityDTO>();
-        CreateMap<DetachedTimeEntry, DetachedTimeEntryDTO>();
+
+        CreateMap<BaseTimeEntry, TimeEntryDTO>()
+            .Include<TimeEntry, TimeEntryDTO>();
         CreateMap<TimeEntry, TimeEntryDTO>();
+
+        CreateMap<BaseTimeEntry, DetachedTimeEntryDTO>()
+            .Include<DetachedTimeEntry, DetachedTimeEntryDTO>();
+        CreateMap<DetachedTimeEntry, DetachedTimeEntryDTO>();
+
         CreateMap<XylosUser, XylosUserDTO>();
     }
 }
