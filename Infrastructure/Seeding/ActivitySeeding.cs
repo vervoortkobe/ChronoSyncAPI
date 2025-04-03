bd6c49c7c74ff1d0d76557895e217002bbe4a913
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-04-03 14:39:15 +0200

    Fix seeding

--- Infrastructure/Seeding/ActivitySeeding.cs
+++ Infrastructure/Seeding/ActivitySeeding.cs
@@ -13,30 +13,19 @@ namespace Infrastructure.Seeding
 
             var activities = new List<Activity>
             {
+                // This week
+                // Effort
                 new()
                 {
                     Id = ObjectId.GenerateNewId().ToString(),
-                    XylosUser = users[0],
+                    XylosUser = users[1],
                     Organisation = "Consult Contract AP Hogeschool",
                     Project = "GalAPagos",
                     Location = "Campus Ellerman",
-                    StartDate = new DateTime(2025, 3, 24),
-                    EndDate = new DateTime(2025, 3, 28),
+                    StartDate = new DateTime(2025, 3, 31),
+                    EndDate = new DateTime(2025, 4, 4),
                     HoursToSpend = 4,
-                    CalculatedMinutesSpent = 0,
-                    Type = ActivityType.EFFORT
-                },
-                new()
-                {
-                    Id = ObjectId.GenerateNewId().ToString(),
-                    XylosUser = users[1],
-                    Organisation = "Meeting Company Xylosophos",
-                    Project = "OmniBus",
-                    Location = "Meeting Room Obelix",
-                    StartDate = new DateTime(2025, 3, 25),
-                    EndDate = new DateTime(2025, 3, 27),
-                    HoursToSpend = 8,
-                    CalculatedMinutesSpent = 446,
+                    CalculatedMinutesSpent = 252,
                     Type = ActivityType.EFFORT
                 },
                 new()
@@ -45,11 +34,38 @@ namespace Infrastructure.Seeding
                     XylosUser = users[1],
                     Organisation = "Consult Contract AP Hogeschool",
                     Project = "GalAPagos",
-                    Location = "Campus Ellerman",
-                    StartDate = new DateTime(2025, 3, 26),
-                    EndDate = new DateTime(2025, 3, 26),
-                    HoursToSpend = 5,
-                    CalculatedMinutesSpent = 278,
+                    Location = "Campus Viaduct",
+                    StartDate = new DateTime(2025, 3, 31),
+                    EndDate = new DateTime(2025, 4, 4),
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 135,
+                    Type = ActivityType.EFFORT
+                },
+                // Time
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Software Company Thorios",
+                    Project = "ThermoApp",
+                    Location = "Antwerpselei 43, 2000 Antwerp",
+                    StartDate = new DateTime(2025, 3, 31),
+                    EndDate = new DateTime(2025, 3, 31),
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 250,
+                    Type = ActivityType.TIME
+                },
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Vervoersmaatschappij DeLijn",
+                    Project = "FlexApp",
+                    Location = "Bushalte Roosevelt A3, 2000 Antwerp",
+                    StartDate = new DateTime(2025, 4, 1),
+                    EndDate = new DateTime(2025, 4, 1),
+                    HoursToSpend = 3,
+                    CalculatedMinutesSpent = 198,
                     Type = ActivityType.TIME
                 },
                 new()
@@ -57,14 +73,82 @@ namespace Infrastructure.Seeding
                     Id = ObjectId.GenerateNewId().ToString(),
                     XylosUser = users[1],
                     Organisation = "Meeting Company Xylosophos",
-                    Project = "OmniBus",
+                    Project = "ChronoSync",
+                    Location = "Meeting Room Panoramix",
+                    StartDate = new DateTime(2025, 3, 31),
+                    EndDate = new DateTime(2025, 3, 31),
+                    HoursToSpend = 2,
+                    CalculatedMinutesSpent = 123,
+                    Type = ActivityType.TIME
+                },
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Meeting Company Xylosophos",
+                    Project = "ChronoSync",
                     Location = "Meeting Room Obelix",
                     StartDate = new DateTime(2025, 4, 2),
                     EndDate = new DateTime(2025, 4, 2),
-                    HoursToSpend = 8,
-                    CalculatedMinutesSpent = 0,
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 245,
                     Type = ActivityType.TIME
-                }
+                },
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Meeting Company Xylosophos",
+                    Project = "OmniBus",
+                    Location = "Meeting Room Idefix",
+                    StartDate = new DateTime(2025, 4, 2),
+                    EndDate = new DateTime(2025, 4, 2),
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 280,
+                    Type = ActivityType.TIME
+                },
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Garden Company Flexios",
+                    Project = "Flox",
+                    Location = "Vlindertuinstraat 1, 1940 Berchem",
+                    StartDate = new DateTime(2025, 4, 2),
+                    EndDate = new DateTime(2025, 4, 2),
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 241,
+                    Type = ActivityType.TIME
+                },
+                // Previous week
+                // Effort
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Consult Contract AP Hogeschool",
+                    Project = "GalAPagos",
+                    Location = "Campus Viaduct",
+                    StartDate = new DateTime(2025, 3, 24),
+                    EndDate = new DateTime(2025, 3, 27),
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 245,
+                    Type = ActivityType.EFFORT
+                },
+                /// Time
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    XylosUser = users[1],
+                    Organisation = "Software Company Thorios",
+                    Project = "ThermoApp",
+                    Location = "Antwerpselei 43, 2000 Antwerp",
+                    StartDate = new DateTime(2025, 3, 27),
+                    EndDate = new DateTime(2025, 3, 27),
+                    HoursToSpend = 4,
+                    CalculatedMinutesSpent = 243,
+                    Type = ActivityType.TIME
+                },
             };
 
             if (collection.CountDocuments(FilterDefinition<Activity>.Empty) <= 0)
--- Infrastructure/Seeding/TimeEntrySeeding.cs
+++ Infrastructure/Seeding/TimeEntrySeeding.cs
@@ -16,31 +16,128 @@ namespace Infrastructure.Seeding
                 new()
                 {
                     Id = ObjectId.GenerateNewId().ToString(),
-                    Activity = activities[1],
-                    Date = new DateTime(2025, 3, 9),
+                    Activity = activities[0],
+                    Date = new DateTime(2025, 3, 31),
                     StartTime = DateTime.Now,
-                    EndTime = DateTime.Now.AddMinutes(334),
+                    EndTime = DateTime.Now.AddMinutes(224),
                     Break = 37,
-                    Description = "Meeting with company Xylosophos"
+                    Description = "Consulting AP Hogeschool"
+                },
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[0],
+                    Date = new DateTime(2025, 4, 1),
+                    Duration = 65,
+                    Description = "Consulting AP Hogeschool"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[1],
+                    Date = new DateTime(2025, 4, 1),
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddMinutes(71),
+                    Break = 4,
+                    Description = "Consulting AP Hogeschool"
                 },
                 new()
                 {
                     Id = ObjectId.GenerateNewId().ToString(),
                     Activity = activities[1],
-                    Date = new DateTime(2025, 3, 10),
-                    Duration = 146,
-                    Description = "Meeting with company Xylosophos"
+                    Date = new DateTime(2025, 4, 2),
+                    Duration = 68,
+                    Description = "Consulting AP Hogeschool"
                 },
+
                 new()
                 {
                     Id = ObjectId.GenerateNewId().ToString(),
                     Activity = activities[2],
-                    Date = new DateTime(2025, 3, 11),
+                    Date = new DateTime(2025, 3, 31),
                     StartTime = DateTime.Now,
-                    EndTime = DateTime.Now.AddMinutes(306),
-                    Break = 28,
-                    Description = "Consulting AP Hogeschool"
-                }
+                    EndTime = DateTime.Now.AddMinutes(302),
+                    Break = 52,
+                    Description = "Sprint meeting ThermoApp"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[3],
+                    Date = new DateTime(2025, 4, 1),
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddMinutes(220),
+                    Break = 22,
+                    Description = "Vervoersplan wijzigen"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[4],
+                    Date = new DateTime(2025, 4, 1),
+                    Duration = 123,
+                    Description = "Overleg ChronoSync applicatie"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[5],
+                    Date = new DateTime(2025, 3, 31),
+                    Duration = 245,
+                    Description = "Analyse ChronoSync applicatie"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[6],
+                    Date = new DateTime(2025, 4, 2),
+                    Duration = 280,
+                    Description = "Bussen bekijken"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[7],
+                    Date = new DateTime(2025, 4, 2),
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddMinutes(271),
+                    Break = 30,
+                    Description = "Grasjes planten"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[8],
+                    Date = new DateTime(2025, 3, 25),
+                    Duration = 183,
+                    Description = "GalAPagos programmeren"
+                },
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[8],
+                    Date = new DateTime(2025, 3, 26),
+                    Duration = 62,
+                    Description = "Database GalAPagos opzetten"
+                },
+
+                new()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Activity = activities[9],
+                    Date = new DateTime(2025, 3, 27),
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddMinutes(264),
+                    Break = 21,
+                    Description = "Grasjes planten"
+                },
             };
 
             if (collection.CountDocuments(FilterDefinition<TimeEntry>.Empty) <= 0)
